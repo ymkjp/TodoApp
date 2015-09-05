1cc231fae3b7bf5a9b6be5ce0fabc44274fddf78
Kenta YAMAMOTO <lq33xxx@gmail.com>
2015-09-06 00:29:17 +0900

    Setup MagicalRecord

---
+++ TodoApp/TodoApp-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TodoApp-Bridging-Header.h
+//  TodoApp
+//
+//  Created by YAMAMOTOKenta on 9/6/15.
+//  Copyright (c) 2015 ymkjp. All rights reserved.
+//
+
+#ifndef TodoApp_TodoApp_Bridging_Header_h
+#define TodoApp_TodoApp_Bridging_Header_h
+
+#import "CoreData+MagicalRecord.h"
+
+#endif
